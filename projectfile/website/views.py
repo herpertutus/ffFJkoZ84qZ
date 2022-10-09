4b6561b1e5a5c8b9173dbc17f65e1b13a6370db6
herpertutus <61857913+herpertutus@users.noreply.github.com>
2022-10-09 13:55:57 +1000

    page templates
    made all of the page templates we will need for now.
    index, events, contact us (might throw away later), account

--- projectfile/website/views.py
+++ projectfile/website/views.py
@@ -7,6 +7,18 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@bp.route('/events')
+def allevents():
+    return render_template('events.html')
+
+@bp.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@bp.route('/account')
+def account():
+    return render_template('account.html')
+
 @bp.route('/test')
 def test():
     return '<h1>test route<h1>'
