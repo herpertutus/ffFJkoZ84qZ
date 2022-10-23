e315a224dffd65809a3a7611857aef1cf31ce196
herpertutus <61857913+herpertutus@users.noreply.github.com>
2022-10-23 12:45:47 +1000

    conflict resolution

--- projectfile/main.py
+++ projectfile/main.py
@@ -10,4 +10,4 @@ from flask import Blueprint,render_template, redirect, url_for, request, flash
 #Handling error 404 and displaying relevant web page
 @napp.errorhandler(404)
 def internal_error(error):
-    return render_template('<h3>ouh ouh poopoo stiiiinky</h3>')
+    return render_template('<h3>error</h3>')
